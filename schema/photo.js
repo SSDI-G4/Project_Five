d97fc59ca38fc67b463b98021efc8745214852d3
Pranav Bakale <pbakale@charlotte.edu>
2024-12-03 21:16:31 -0500

    Update photo.js

--- schema/photo.js
+++ schema/photo.js
@@ -26,7 +26,6 @@ const photoSchema = new mongoose.Schema({
   user_id: mongoose.Schema.Types.ObjectId,
   // Array of comment objects representing the comments made on this photo.
   comments: [commentSchema],
-<<<<<<< HEAD
   liked_by: {
     type: [mongoose.Schema.Types.ObjectId],
     ref: "User",
@@ -35,8 +34,6 @@ const photoSchema = new mongoose.Schema({
     type: [mongoose.Schema.Types.ObjectId],
     ref: "User",
   },
-=======
->>>>>>> f7771d4aa7fc7fccb65a53461bc89c08c4a7b745
 });
 
 /**
